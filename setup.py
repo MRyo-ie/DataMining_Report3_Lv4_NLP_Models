f5d051fb44529e092b7b2e13873ddd46be7a3cbf
MRyo-ie <e165738@ie.u-ryukyu.ac.jp>
2019-12-06 09:10:31 +0900

    【init】

---
+++ setup.py
@@ -0,0 +1,26 @@
+
+import argparse
+import os
+
+
+parser = argparse.ArgumentParser()
+###  [データ] train, test ファイルの場所  ###
+parser.add_argument('--data_dir', type=str, default='./datas/articles_livedoor',
+                    help='データの保存先（ディレクトリのパス）')
+###  [モデル]  ###
+parser.add_argument('--model_dir', type=str, default='./models/ckpts',
+                    help='Base directory for the model.')
+parser.add_argument('--pre_trained_model', type=str, default='./models/org_resnet/resnet_v2_50.ckpt',
+                    help='Path to the pre-trained model checkpoint.')
+parser.add_argument('--init_model_dir', action='store_true',
+                    help='Whether to clean up the model directory if present.')
+parser.add_argument('--base_architecture', type=str, default='resnet_v2_50',
+                    choices=['resnet_v2_50', 'resnet_v2_101'],
+                    help='The architecture of base Resnet building block.')
+
+
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
