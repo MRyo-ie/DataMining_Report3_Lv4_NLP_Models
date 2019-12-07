23d8467cbc37f0a570411a3a7ea422196b9ebb54
MRyo-ie <e165738@ie.u-ryukyu.ac.jp>
2019-12-08 05:10:51 +0900

    【v1.00】「完成！」

--- models/LanguageModel/Co_Matrix.py
+++ models/LanguageModel/Co_Matrix.py
@@ -7,15 +7,17 @@ class CoMatrix():
     """
     文章ベクトル
     　＝　文書中に現れた全単語の単語ベクトルを足し合わせ、総単語数で割る
-    　＝　文書中に現れた単語のベクトルを、全単語分 計算する。
+    　＝　文書中に現れた単語のベクトルを、全単語 計算する。
     　　　 → 文章を入力すると、その文章に含まれる全単語ベクトルを足し合わせ、総単語数で割った値を返す。
     """
-    def __init__(self, token2id):
+    def __init__(self, token2id, tknd_all_sentences, window_size=2):
         # 文書中に現れた単語のベクトルを、全単語分 計算する。
         vocab_size = len(token2id)
         self.co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
         self.w2i = token2id
-
+        # 初期化時に、共起行列ベクトルを完成させておく。
+        for tknd_s in tqdm(tknd_all_sentences):
+            self.update(tknd_s)
 
     def update(self, tknd_corpus, window_size=2):
         """
@@ -32,11 +34,10 @@ class CoMatrix():
             共起判定の範囲。ウィンドウサイズ
         """
         corpus_size = len(tknd_corpus)
-
         try:
             for ti in range(corpus_size):
                 for ci in range(1, window_size + 1):
-                    if ti-ci > 0:
+                    if ti-ci >= 0:
                         self.update_co_matrix(tknd_corpus[ti], tknd_corpus[ti - ci])
                     if ti+ci < corpus_size:
                         self.update_co_matrix(tknd_corpus[ti], tknd_corpus[ti + ci])
@@ -48,43 +49,43 @@ class CoMatrix():
     def update_co_matrix(self, target_w, context_w):
         self.co_matrix[self.w2i[target_w], self.w2i[context_w]] += 1
 
-    def embed_co_marrix(self):
+    def embed_co_marrix(self, tknd_corpus):
         """
         全単語ベクトルを足し合わせ、総単語数で割る
         """
+        sum_vec = np.zeros_like(self.co_matrix[1,:])
+        for token in tknd_corpus:
+            sum_vec += self.co_matrix[self.w2i[token],:]
+            # print(sum_vec)
+        sum_vec = sum_vec / len(tknd_corpus)  # 総単語数で割る
+        return sum_vec
 
 
 
 if __name__ == "__main__":
-    # word_list=['natural', 'language', 'text', 'count', 'word', 'tknd_corpus', 'python', 'program', 'programs']
-    # token2id = pre.identify_some_tokens(word_list)
-    # vocab_size = len(word_list)
-    # co_matrix = None
-    # for text in tqdm(docs):
-    #     tknd_corpus = build_tknd_corpus(text, token2id,   word_list)
-    #     co_matrix = update_matrix(tknd_corpus, vocab_size, co_matrix, window_size=2)
-
-    # # cos類似度適用前
-    # print('\n')
-    # pd.DataFrame(co_matrix, index=word_to_id.keys(), columns=word_to_id.keys())
-
     try:  # 無視する単語は飛ばす
         test_dict = {'a': 3, 'b': 45}
         test_dict['c']
     except KeyError:
-        print('無視する単語は飛ばす')
+        print('無視する単語は飛ばす\n')
 
 
     # test
-    tknd_corpus = [['吾輩', 'は', '猫', 'である'], ['名前', 'は', 'まだ', 'ない']]
+    tknd_corpus = [['吾輩', 'は', '猫', 'である', '。'],
+                    ['名前', 'は', 'まだ', 'ない', '。'],
+                    ['吾輩', 'は', 'ここ', 'で', '始めて', '人間', 'という', 'もの', 'を', '見た', '。']]
     # まず word2id を作る。
     token_set = set()
     for tknd_s in tqdm(tknd_corpus):
-        token_set = pre.add_tokens_set(token_set, tknd_s)   # Token の集合（set） を作成。（set は、dict のkeyだけ版）
+        token_set = token_set | set(tknd_s)   # Token の集合（set） を作成。（set は、dict のkeyだけ版）
     data_w2i = pre.identify_some_tokens(token_set)
     print(data_w2i)
     # word2id を元に、共起行列を作る。
-    cm = CoMatrix(data_w2i)
-    for tknd_s in tqdm(tknd_corpus):
-        cm.update(tknd_s)  # Token の集合（set） を作成。（set は、dict のkeyだけ版）
+    cm = CoMatrix(data_w2i, tknd_corpus)
     print(cm.co_matrix)
+    # 共起行列を元に、入力文（章）をベクトル化
+    for tknd_s in tknd_corpus:
+        print('--------------------------')
+        vec = cm.embed_co_marrix(tknd_s)
+        print('sentence : {} \nlen : {}  \nvec : \n{}'.format(tknd_s, len(vec), vec))
+
--- models/LanguageModel/pre.py
+++ models/LanguageModel/pre.py
@@ -3,14 +3,6 @@
 # Embedding する前に行う処理を集めた。
 
 ###  Token（単語）に ID を振る  ###
-# sentenceに含まれる、全ての Token の集合を取得する。
-# 重複が削除された、Token の集合（set）が返ってくる。
-def add_tokens_set(token_set:set, add_tokens_list):
-    # 集合(set) に変換して、重複を削除する。
-    # 和集合を取る。（重複が削除された状態の、Token の集合が得られる）
-    return token_set | set(add_tokens_list)
-
-
 # 指定した word （some_words に含まれるもののみ）にIDを振る。
 # 言い換えると、some_words に ID を振る。
 def identify_some_tokens(some_words):
